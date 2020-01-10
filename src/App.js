13ca326c273c8c2ecdebbe058338b92e0cc7dfc7
Dinh Bac <nguyendinhbac99@gmail.com>
2020-01-10 15:45:52 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,288 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import uuidv4 from "uuid/v4";
+import SweetAlert from "sweetalert-react";
+import "sweetalert/dist/sweetalert.css";
+import Title from "./components/Title";
+import Search from "./components/Search";
+import Sort from "./components/Sort";
+import ListItem from "./components/ListItem";
+import Form from "./components/Form";
+import Items from "./mockdata/Items";
+import Item from "./components/Item";
+import ItemEdit from "./components/ItemEdit";
+import { orderBy as orderByld } from "lodash";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <SweetAlert
+          show={this.state.showAlert}
+          title="Delete Item"
+          text={this.state.titleAlert}
+          showCancelButton
+          onOutsideClick={() => this.setState({ showAlert: false })}
+          onEscapeKey={() => this.setState({ showAlert: false })}
+          onCancel={() => this.setState({ showAlert: false })}
+          onConfirm={() => this.handleDeleteItem()}
+        />
+        <Title />
+        <div className="row">
+          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            <Search
+              handleClickClear={this.handleClickClear}
+              valueSearch={this.state.valueSearch}
+              handleSearchInputChange={this.handleSearchInputChange}
+            />
+          </div>
+          <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+            <Sort
+              sortType={this.state.sortType}
+              sortOrder={this.state.sortOrder}
+              handleSort={this.handleSort}
+            />
+          </div>
+          <div className="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+            <button
+              type="button"
+              className="btn btn-info btn-block marginB10"
+              onClick={this.handleShowForm}
+            >
+              {this.state.showForm ? "Close Item" : "Add Item"}
+            </button>
+          </div>
+        </div>
+        <div className="row marginB10">
+          <div className="col-md-offset-7 col-md-5">
+            <Form
+              showForm={this.state.showForm}
+              arrayLevel={this.state.arrayLevel}
+              valueItem={this.state.valueItem}
+              handleFormInputChange={this.handleFormInputChange}
+              levelItem={this.state.levelItem}
+              handleFormSelectChange={this.handleFormSelectChange}
+              handleFormClickCancel={this.handleFormClickCancel}
+              handleFormClickSubmit={this.handleFormClickSubmit}
+            />
+          </div>
+        </div>
+        <ListItem renderItem={this.renderItem} />
+      </div>
+    );
+  }
+  constructor(props) {
+    super(props);
+    let arrayLevel = [];
+    if (Items.length > 0) {
+      for (let i = 0; i < Items.length; i++) {
+        if (arrayLevel.indexOf(Items[i].level) === -1) {
+          arrayLevel.push(Items[i].level);
+        }
+      }
+    }
+    arrayLevel.sort(function(a, b) {
+      return a - b;
+    });
+    this.state = {
+      items: Items,
+      showAlert: false,
+      titleAlert: "",
+      idAlert: "",
+      indexEdit: 0,
+      idEdit: "",
+      nameEdit: "",
+      levelEdit: 0,
+      arrayLevel: arrayLevel,
+      showForm: false,
+      valueItem: "",
+      levelItem: 0,
+      sortType: "",
+      sortOrder: "",
+      valueSearch: ""
+    };
+  }
+
+  renderItem = () => {
+    let {
+      items,
+      idEdit,
+      indexEdit,
+      nameEdit,
+      levelEdit,
+      arrayLevel
+    } = this.state;
+    if (items.length === 0) {
+      return <Item item={0} />;
+    }
+    return items.map((item, index) => {
+      if (item.id === idEdit) {
+        return (
+          <ItemEdit
+            key={index}
+            indexEdit={indexEdit}
+            nameEdit={nameEdit}
+            levelEdit={levelEdit}
+            arrayLevel={arrayLevel}
+            handleEditClickCancel={this.handleEditClickCancel}
+            handleEditInputChange={this.handleEditInputChange}
+            handleEditSelectChange={this.handleEditSelectChange}
+            handleEditClickSubmit={this.handleEditClickSubmit}
+          />
+        );
+      }
+      return (
+        <Item
+          item={item}
+          index={index}
+          key={index}
+          handleShowAlert={this.handleShowAlert}
+          handleEditItem={this.handleEditItem}
+        />
+      );
+    });
+  };
+
+  handleShowAlert = item => {
+    this.setState({
+      showAlert: true,
+      titleAlert: item.name,
+      idAlert: item.id
+    });
+  };
+
+  handleDeleteItem = () => {
+    let { idAlert, items } = this.state;
+    if (items.length > 0) {
+      for (let i = 0; i < items.length; i++) {
+        if (items[i].id === idAlert) {
+          items.splice(i, 1);
+          break;
+        }
+      }
+    }
+    this.setState({
+      showAlert: false,
+      titleAlert: "",
+      idAlert: ""
+    });
+  };
+
+  handleEditItem = (index, item) => {
+    this.setState({
+      indexEdit: index,
+      idEdit: item.id,
+      nameEdit: item.name,
+      levelEdit: item.level
+    });
+  };
+
+  handleEditClickCancel = () => {
+    this.setState({
+      idEdit: ""
+    });
+  };
+
+  handleEditInputChange = value => {
+    this.setState({
+      nameEdit: value
+    });
+  };
+
+  handleEditSelectChange = value => {
+    this.setState({
+      levelEdit: value
+    });
+  };
+
+  handleEditClickSubmit = () => {
+    let { items, idEdit, nameEdit, levelEdit } = this.state;
+    if (items.length > 0) {
+      for (let i = 0; i < items.length; i++) {
+        if (items[i].id === idEdit) {
+          items[i].name = nameEdit;
+          items[i].level = +levelEdit;
+          break;
+        }
+      }
+    }
+    this.setState({
+      idEdit: ""
+    });
+  };
+
+  handleShowForm = () => {
+    this.setState({
+      showForm: !this.state.showForm
+    });
+  };
+
+  handleFormInputChange = value => {
+    this.setState({
+      valueItem: value
+    });
+  };
+
+  handleFormSelectChange = value => {
+    this.setState({
+      levelItem: value
+    });
+  };
+
+  handleFormClickCancel = () => {
+    this.setState({
+      valueItem: "",
+      levelItem: 0
+    });
+  };
+
+  handleFormClickSubmit = () => {
+    let { valueItem, levelItem } = this.state;
+    if (valueItem.trim() === "") return false;
+    let newItem = {
+      id: uuidv4(),
+      name: valueItem,
+      level: +levelItem
+    };
+    Items.push(newItem);
+    this.setState({
+      items: Items,
+      valueItem: "",
+      levelItem: 0,
+      showForm: false
+    });
+  };
+
+  handleSort = (sortType, sortOrder) => {
+    this.setState({
+      sortType: sortType,
+      sortOrder: sortOrder
+    });
+    let { items } = this.state;
+    this.setState({
+      items: orderByld(items, [sortType], [sortOrder])
+    });
+  };
+
+  handleClickClear = () => {
+    this.setState({
+      items: Items,
+      valueSearch: ""
+    });
+  };
+
+  handleSearchInputChange = value => {
+    if (value.trim() === "") {
+      this.setState({ items: Items, valueSearch: "" });
+      return false;
+    }
+    let filteredItems = Items.filter(item => {
+      return item.name.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+    });
+    this.setState({
+      items: filteredItems,
+      valueSearch: value
+    });
+  };
 }
 
 export default App;
---
+++ src/components/Sort.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+class Sort extends Component {
+  renderSort = () => {
+    let { sortType, sortOrder } = this.props;
+    if (sortType !== "" && sortOrder !== "") {
+      return (
+        <span className="label label-success label-medium text-uppercase">
+          {sortType} - {sortOrder}
+        </span>
+      );
+    }
+  };
+
+  handleClick = (sortType, sortOrder) => {
+    this.props.handleSort(sortType, sortOrder);
+  };
+
+  render() {
+    return (
+      <div className="dropdown">
+        <button
+          className="btn btn-default dropdown-toggle marginR5"
+          type="button"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="true"
+        >
+          Sort by <span className="caret" />
+        </button>
+        <ul className="dropdown-menu">
+          <li onClick={() => this.handleClick("name", "asc")}>
+            <a role="button" className="text-uppercase">
+              Name ASC
+            </a>
+          </li>
+          <li onClick={() => this.handleClick("name", "desc")}>
+            <a role="button" className="text-uppercase">
+              Name DESC
+            </a>
+          </li>
+          <li role="separator" className="divider"></li>
+          <li onClick={() => this.handleClick("level", "asc")}>
+            <a role="button" className="text-uppercase">
+              Level ASC
+            </a>
+          </li>
+          <li onClick={() => this.handleClick("level", "desc")}>
+            <a role="button" className="text-uppercase">
+              Level DESC
+            </a>
+          </li>
+        </ul>
+        {this.renderSort()}
+      </div>
+    );
+  }
+}
+
+export default Sort;
