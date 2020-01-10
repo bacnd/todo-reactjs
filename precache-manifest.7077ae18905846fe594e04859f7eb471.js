5b29350a2e8332cc6955ca5a8b506c8728d35d70
Dinh Bac <nguyendinhbac99@gmail.com>
2020-01-10 15:50:07 +0700

    Updates

---
+++ precache-manifest.7077ae18905846fe594e04859f7eb471.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc996afabb451357a738dc27a1979dd",
+    "url": "/todo-reactjs/index.html"
+  },
+  {
+    "revision": "f894db7f43a4d372d354",
+    "url": "/todo-reactjs/static/css/2.fb8a3fcb.chunk.css"
+  },
+  {
+    "revision": "ff6691c04a830c13d888",
+    "url": "/todo-reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f894db7f43a4d372d354",
+    "url": "/todo-reactjs/static/js/2.37646064.chunk.js"
+  },
+  {
+    "revision": "da01034c70d479d890a70d42d48c6305",
+    "url": "/todo-reactjs/static/js/2.37646064.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff6691c04a830c13d888",
+    "url": "/todo-reactjs/static/js/main.cd5b8ccb.chunk.js"
+  },
+  {
+    "revision": "ba417d9a918fb51fd1ff",
+    "url": "/todo-reactjs/static/js/runtime-main.e576db05.js"
+  }
+]);
